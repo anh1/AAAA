6f37da9bd038c368a16940ee3c292c8cdbe31db5
anh <atang245@gmail.com>
2012-02-07 13:02:04 -0600

    update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SbtnApp::Application.config.secret_token = 'ef61e5dd79f193e9b68e49ae20dbcb738f2177f7d6711528553a718489fc4d4bd83298ccee3c9dbe226e89721cfe3a9accafaf1b92a75a8cfb346f9f83803267'
+SbtnApp::Application.config.secret_token = '576e8e9cd0ad4e19742344623db16fb4109eaecbd739cbbe283ffc26646e6b708fdc7755098640ae931508ae3b6b2b1c1b7d06d8bbdfdf604e274c2794f9249c'
