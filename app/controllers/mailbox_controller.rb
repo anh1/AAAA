b94d4eb2973937aec7e37765b3e2ad59d00a2ea1
Anh Tang <atang245@gmail.com>
2012-03-31 14:52:36 -0500

    Apri 1, 2012

--- app/controllers/mailbox_controller.rb
+++ app/controllers/mailbox_controller.rb
@@ -18,13 +18,12 @@ layout "mailbox"
   def show
     @folder ||= @current_user.folders.find(params[:id])
     @not_deleted=@folder.messages.where(["deleted IS NULL OR deleted = ?", false])
-    @messages = @not_deleted.paginate :all, :per_page => 10, :page => params[:page], :include => :message, :order => "messages.created_at DESC"
-  end
+    @messages=@not_deleted.paginate(:page => params[:page],:per_page => 10, :page => params[:page], :include => :message, :order => "messages.created_at DESC")
   
  def trash
     @folder = Struct.new(:name, :user_id).new("Trash", @current_user.id)
     @deleted = @current_user.received_messages.where(["deleted IS NULL OR deleted = ?", true])
-    @messages = @deleted.paginate :all, :per_page => 10, :page => params[:page], :include => :message, :order => "messages.created_at DESC"
+    @messages = @deleted.paginate(:page => params[:page],:per_page => 10, :page => params[:page], :include => :message, :order => "messages.created_at DESC")
     render :action => "show"
   end
 
